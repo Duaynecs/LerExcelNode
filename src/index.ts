5840aa2a2fb2fde9f4981f9027e00fa799258023
Duayne Santos <duayne@aramo.com.br>
2023-04-22 16:45:36 -0300

    criando funcao para ler o arquivo excel e mostrar no console.log

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+import * as XLSX from "xlsx";
 
+
+const arq = XLSX.readFile('../101-Grupos_Alimento_Básico-Martins.xlsx');
+
+let dados: any = [];
+
+const aba = arq.SheetNames;
+
+for (let i = 0; i < aba.length; i++) {
+  const temp = XLSX.utils.sheet_to_json(arq.Sheets[arq.SheetNames[1]])
+
+  temp.forEach((res) => {
+    dados.push(res)
+  })
+}
+
+console.log(dados);
